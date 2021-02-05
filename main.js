ca2c043c2c2c7799cbca384f224901856a5f7aba
gyungchang <rudckd135@naver.com>
2021-02-05 16:07:35 +0900

    First UI Web

---
+++ main.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/Desktop___1.html';
+    }
+    else if(request.url == '/Desktop___2.html'){
+      url = '/Desktop___2.html';
+    }
+    else if(request.url == '/Desktop___3.html'){
+      url = '/Desktop___3.html';
+    }
+    if(request.url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+});
+app.listen(3000);
